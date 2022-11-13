cd74c729da30402031b4e0325e763a66aa5eb87e
Danilo Paggi <danilopaggi@gmail.com>
2022-11-13 14:30:14 -0300

    Fix code style in DateTime class

--- src/utils/date_time.py
+++ src/utils/date_time.py
@@ -2,7 +2,6 @@ import datetime
 
 
 class DateTime(object):
-
     @staticmethod
     def is_valid_date(year: str, month: str, day: str):
         try:
--- src/utils/test_date_time.py
+++ src/utils/test_date_time.py
@@ -7,7 +7,9 @@ class TestDateTime:
     def test_return_true_for_a_valid_date(self):
         current_time = datetime.datetime.now()
 
-        assert DateTime.is_valid_date(f"{current_time.year}", f"{current_time.month}", f"{current_time.day}")
+        assert DateTime.is_valid_date(
+            f"{current_time.year}", f"{current_time.month}", f"{current_time.day}"
+        )
         assert DateTime.is_valid_date("2020", "02", "29")
 
     def test_return_false_for_an_invalid_date(self):
