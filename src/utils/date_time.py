6bf3ce72a5d97d12154e638632e39558ae6ed03d
Danilo Paggi <danilopaggi@gmail.com>
2022-11-13 12:54:37 -0300

    Add util class to help with date validation

---
+++ src/utils/date_time.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+class DateTime(object):
+
+    @staticmethod
+    def is_valid_date(year: str, month: str, day: str):
+        try:
+            date = datetime.datetime(year=int(year), month=int(month), day=int(day))
+        except Exception as e:
+            return False
+        else:
+            return True
---
+++ src/utils/test_date_time.py
@@ -0,0 +1,17 @@
+import datetime
+
+from src.utils.date_time import DateTime
+
+
+class TestDateTime:
+    def test_return_true_for_a_valid_date(self):
+        current_time = datetime.datetime.now()
+
+        assert DateTime.is_valid_date(f"{current_time.year}", f"{current_time.month}", f"{current_time.day}")
+        assert DateTime.is_valid_date("2020", "02", "29")
+
+    def test_return_false_for_an_invalid_date(self):
+        assert not DateTime.is_valid_date("", "", "")
+        assert not DateTime.is_valid_date("2022", "13", "01")
+        assert not DateTime.is_valid_date("2022", "02", "31")
+        assert not DateTime.is_valid_date("A", "02", "31")
